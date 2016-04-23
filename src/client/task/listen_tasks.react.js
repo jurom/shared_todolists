45d31dba4f61295ae4b5e3a7f15cc3517efdb0cb
Juraj Machac <jurajmchc@gmail.com>
2016-04-23 11:58:12 +0200

    removed weird code

--- src/client/task/listen_tasks.react.js
+++ src/client/task/listen_tasks.react.js
@@ -10,23 +10,3 @@ export const ListenFriendTasks = listenFirebase(
   (props) => props.firebase.child(`task/${props.friendId}/${props.uid}`),
   (e, props, data) => props.dispatch(actions.onFriendTasks, [props.friendId, props.uid, data.val() || {}])
 )
-
-
-function find(st, end, s) {
-  var mid = Math.floor((st + end) / 2)
-  if (s === mid) return mid
-  if (s > mid) return find(mid, end)
-  if (s < mid) return find(st, mid)
-}
-
-function findEnd(s) {
-  return gallop(1, s)
-}
-
-function gallop(k, s) {
-  return k < s ? gallop(2 * k, s) : k
-}
-
-function guess(s) {
-  return find(0, findEnd(s), s)
-}
